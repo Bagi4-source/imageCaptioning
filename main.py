2470150109b4a922b351b421d40f366173b007dc
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2025-01-03 21:35:24 +0300

    upload project

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from PIL import Image
+from deep_translator import GoogleTranslator
+
+MAX_LENGTH = 20
+NUM_BEAMS = 4
+
+translator = GoogleTranslator(source='en', target='ru')
+model_path = "model"
+model = VisionEncoderDecoderModel.from_pretrained(model_path)
+image_processor = ViTImageProcessor.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+
+def generate_caption(_image: Image):
+    pixel_values = image_processor(images=_image,
+                                   return_tensors="pt").pixel_values
+    output_ids = model.generate(pixel_values,
+                                max_length=MAX_LENGTH,  # максимальная длина вывода
+                                num_beams=NUM_BEAMS,  # число лучей для beam search
+                                early_stopping=True)
+
+    caption = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    return caption
+
+
+images = [Image.open(f"images/{x}") for x in os.listdir("images")]
+for i, image in enumerate(images):
+    generated_caption = generate_caption(image)
+    translated = translator.translate(generated_caption)
+    print(f"Описание картинки [{i}]:", translated)
